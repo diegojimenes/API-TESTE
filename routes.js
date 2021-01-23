f2e23a355f11323214592a6f525e1d3b31a33443
Diego jimenes <lligaskate@gmail.com>
2021-01-23 15:55:47 -0300

    rotas basicas

--- routes.js
+++ routes.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import Contas from './controllers/Contas'
 const routes = express.Router();
 
+routes.post('/contas', Contas.store)
+routes.post('/listar-contas', Contas.index)
 export default routes
