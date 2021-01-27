c6cf526a9e3df563fdb5943f895184fe6343557b
Diego jimenes <lligaskate@gmail.com>
2021-01-26 21:35:53 -0300

    adicionando client

---
+++ Client/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload. VAI TOMAR NO CU BOLSONARO
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
